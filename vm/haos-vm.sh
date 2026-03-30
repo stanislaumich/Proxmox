0d2de17a21be324c2fb2e677c0368db8c8f98ca8
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2026-03-30 21:25:44 +0300

    Update haos-vm.sh

--- vm/haos-vm.sh
+++ vm/haos-vm.sh
@@ -117,10 +117,8 @@ function check_root() {
 function pve_check() {
   if ! pveversion | grep -Eq "pve-manager/8.4"; then
     msg_error "This version of Proxmox Virtual Environment is not supported"
-    echo -e "Requires Proxmox Virtual Environment Version 8.1 or later."
-    echo -e "Exiting..."
-    sleep 2
-    exit
+   
+    
 fi
 }
 
